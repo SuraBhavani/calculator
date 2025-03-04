ba485b67102c1c60cbcab32d130c97ebcffaaa1a
SuraBhavani <bhavanireddysura@gmail.com>
2025-03-04 16:07:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function calculate(operation) {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Please enter valid numbers");
+        return;
+    }
+
+    let result;
+    if (operation === "add") {
+        result = num1 + num2;
+    } else if (operation === "subtract") {
+        result = num1 - num2;
+    } else if (operation === "multiply") {
+        result = num1 * num2;
+    } else if (operation === "division") {
+        result = num1 / num2;
+    } else if (operation === "modulo") {
+        result = num1 % num2;
+    }
+
+    document.getElementById("result").textContent = result;
+    console.log(result);
+}
